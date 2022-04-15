8a86560a1c28e3768321e290a23153fe9efc6c5c
henrry <henrrymatituy@gmail.com>
2022-04-14 19:48:36 -0500

    Primer Commit

---
+++ src/main/java/PrimerosWordCounts/WordCountConsola.java
@@ -0,0 +1,41 @@
+package PrimerosWordCounts;
+
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+public class WordCountConsola {
+    public static void main(String[] args) throws Exception {
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        DataSet<String> text = env.fromElements(
+                "Primera frase de mi primer WordCount",
+                "Segunda frase de mi primer WordCount",
+                "Tercera frase de mi primer WordCount",
+                "Cuarta frase de mi primer WordCount"
+        );
+        DataSet<Tuple2<String, Integer>> counts =
+                // split up the lines in pairs (2-tuples) containing: (word,1)
+                text.flatMap(new LineSplitter())
+                        // group by the tuple field "0" and sum up tuple field "1"
+                        .groupBy(0)
+                        .sum(1);
+        counts.print();
+    }
+
+    public static final class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            // normalize and split the line
+            String[] tokens = value.toLowerCase().split("\\W+");
+            // emit the pairs
+            for (String token : tokens) {
+                if (token.length() > 0) {
+                    out.collect(new Tuple2<String, Integer>(token, 1));
+                }
+            }
+        }
+    }
+}
